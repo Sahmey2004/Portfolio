ccbd8888d6f5e9d8af673c01b9e5d02e4eacf4f7
Sahmey <sahmeykhan88@gmail.com>
2025-06-29 17:11:40 +0600

    project updates

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -44,15 +44,20 @@ const Project = () => {
         <div className="mt-10">
             <Card className = 'max-w-fit m-3 md:w-[400px]'>
                 <CardHeader>
-                    <CardTitle>Project 2</CardTitle>
-                   <CardDescription>Java,Python</CardDescription>
+                    <CardTitle>Excel Housing Construction Forecast</CardTitle>
+                   <CardDescription>(Dec 2024)</CardDescription>
                     
                 </CardHeader>
                 <CardContent>
                     <div className="flex flex-col gap-6">
             <div className="grid gap-2">
-                <ul>
-                    <li>Implemented classification technique of supervised learning enhancing real-life dataset prediction accuracy by 96.2%</li>
+                <ul className="list-disc pl-5 space-y-2 text-base">
+                    <li>Applied the PED framework to analyze and organize 60 FRED datasets on new housing construction across 51 states
+</li>
+                    <li>Utilised Linear Regression to predict new housing construction licenses which gave valuable insights to housing investors
+</li>
+                    <li>Developed dynamic charts and an interactive dashboard to aid prospective homeowners in efficient data interpretation
+</li>
                 </ul>
             </div>
 
