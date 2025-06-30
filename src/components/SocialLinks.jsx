d4d039f69795b453133479d9c3edc7643aea53d6
Sahmey <sahmeykhan88@gmail.com>
2025-07-01 04:06:01 +0600

    Added social media icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
-
+import githubIcon from '@/assets/github.png';
+import gmailIcon from '@/assets/gmail.png';
+import linkedinIcon from '@/assets/link.png';
+import instagramIcon from '@/assets/insta.png';
 const SocialLinks = ({ className = "", size = "default" }) => {
   const socialLinks = [
     {
       name: 'LinkedIn',
-      icon: '💼',
+      icon: <span className="flex items-center justify-center w-7 h-7 rounded-full bg-gray-100 border border-gray-300">
+      <img src={linkedinIcon} alt="LinkedIn" className="w-10 h-7 object-contain" />
+    </span>,
       url: 'https://linkedin.com/in/sahmey-raiyan-khan',
       color: 'hover:text-blue-600'
     },
     {
       name: 'GitHub',
-      icon: '💻',
-      url: 'https://github.com/sahmeyraiyan',
+      icon: <span className="flex items-center justify-center w-7 h-7 rounded-full bg-gray-100 border border-gray-300">
+      <img src={githubIcon} alt="GitHub" className="w-5 h-5 object-contain" />
+    </span>,
+      url: 'https://github.com/Sahmey2004',
       color: 'hover:text-gray-800'
     },
     {
       name: 'Email',
-      icon: '📧',
-      url: 'mailto:sahmey.khan@example.com',
+      icon: <span className="flex items-center justify-center w-7 h-7 rounded-full bg-gray-100 border border-gray-300">
+      <img src={gmailIcon} alt="Gmail" className="w-6 h-5 object-contain" />
+    </span>,
+      url: 'mailto:sahmeykhan88@gmail.com',
       color: 'hover:text-red-600'
     },
     {
-      name: 'Twitter',
-      icon: '🐦',
-      url: 'https://twitter.com/sahmeyraiyan',
+      name: 'Instagram',
+      icon: (
+        <span className="flex items-center justify-center w-7 h-7 rounded-full bg-gray-100 border border-gray-300">
+          <img src={instagramIcon} alt="Instagram" className="w-8 h-8 object-contain" />
+        </span>
+      ),
+      url: 'https://www.instagram.com/sahmey.9/',
       color: 'hover:text-blue-400'
     }
   ];
