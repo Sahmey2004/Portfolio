a9a956622358b6a68d2c77fe26b749e297bcade4
Sahmey Raiyan Khan <sahmeykhan88@gmail.com>
2025-08-30 20:27:02 -0400

    Updated Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -113,7 +113,6 @@ const Project = () => {
         </div>
 
         <div className="grid lg:grid-cols-3 xl:grid-cols-5 md:grid-cols-2 gap-8 justify-center">
-        <div className="grid lg:grid-cols-3 xl:grid-cols-6 md:grid-cols-2 gap-8 justify-center">
           {projects.map((project, index) => (
             <div
               key={index}
